9e8d771e94a80655c7d2df8757a4a346e909d26e
Alejandro Companioni <achompas@gmail.com>
2011-09-14 12:04:17 -0300

    naivebayes.prob_class_given_features() (needs a better name...)

--- naivebayes.py
+++ naivebayes.py
@@ -1,5 +1,6 @@
 import sqlite3
 import re
+import math
 
 from classifier import TweetClassifier
 
@@ -21,13 +22,27 @@ class NBClassifier(classifier.TweetClassifier):
     def prob_class_given_features(self, tweet, tweet_class):
         """ 
         Returns the probability that a tweet falls into a class given
-        the features in that tweet (or: P(class | vector of words) ).
+        the features in that tweet (or: P(class | features) ).
+        
+        Given the NB assumption of feature independence, we can simply
+        multiply the probabilities for each feature like so:
+        
+        P(class | features) =              p_x1 * p_x2 * ... * p_xn
+                              ___________________________________________________
+                              (p_x1 * ... * p_xn) + [ (1-p_x1) * ... * (1-p_xn) ]
+                              
+        where p_xi = P(feature i | class). Using natural logs, this can be simplified to:
+        
+        P(class | features) = 1 / (1 + e^nu), nu = sum( ln(1-p_xi) - ln(p_i))
+        
+        (thanks to anonymous Wikipedia editors for this tip: 
+        http://en.wikipedia.org/wiki/Bayesian_spam_filtering#Mathematical_foundation)
         """
         words = self.split_words(tweet)
-        prob = 1
+        nu = 0
         for word in words:
-            prob *= self.get_prob(word, tweet_class)
-        return prob
+            exponent += math.ln(1 - get_prob(word, "D")_ - math.ln(get_prob(word, "D")))
+        return 1 / (1 + exp(nu))
 
 def train(DB, TRAINER):
     """ 
