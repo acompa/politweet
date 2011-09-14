f91c9a0a96494c71758d444d9d4987317dc9c3bb
Alejandro Companioni <achompas@gmail.com>
2011-09-14 12:53:58 -0300

    Naming changes to NBClassifer. See notes.
    Changes:
    1. NBClassifier.prob_class_given_features() renamed to NBClassifier.classify().
    2. naivebayes.classify() folded into naivebayes.main() for clarity.
    3. TweetClassifier.classify_words() renamed to TweetClassifier.train().

--- naivebayes.py
+++ naivebayes.py
@@ -19,7 +19,7 @@ class NBClassifier(classifier.TweetClassifier):
 
         return float(count/total)
 
-    def prob_class_given_features(self, tweet, tweet_class):
+    def classify(self, tweet, tweet_class):
         """ 
         Returns the probability that a tweet falls into a class given
         the features in that tweet (or: P(class | features) ).
@@ -31,26 +31,37 @@ class NBClassifier(classifier.TweetClassifier):
                               ___________________________________________________
                               (p_x1 * ... * p_xn) + [ (1-p_x1) * ... * (1-p_xn) ]
                               
-        where p_xi = P(feature i | class). Using natural logs, this can be simplified to:
+        where p_xi = P(feature | class) aka TweetClassifier.get_prob(). Using 
+        natural logs, this can be simplified to:
         
-        P(class | features) = 1 / (1 + e^nu), nu = sum( ln(1-p_xi) - ln(p_i))
+        P(class | features) = 1 / (1 + e^eta), eta = sum( ln(1-p_xi) - ln(p_i))
+        
+        to avoid floating-point underflow.
         
         (thanks to anonymous Wikipedia editors for this tip: 
         http://en.wikipedia.org/wiki/Bayesian_spam_filtering#Mathematical_foundation)
         """
         words = self.split_words(tweet)
-        nu = 0
+        eta = 0
         for word in words:
             exponent += math.ln(1 - get_prob(word, "D")_ - math.ln(get_prob(word, "D")))
-        return 1 / (1 + exp(nu))
+        return 1 / (1 + exp(eta))
 
-def train(DB, TRAINER):
-    """ 
-    Train the classifier using sample of legislators' tweets.
-
-    TO DO: select rows at random from sample. Partition sample into training
-    and test subsets.
+def main():
     """
+    Main script.
+
+    NOTE: as a future extension, consider one trainer for each congressional
+    body. How do senators, reps tweet?
+    """
+      
+    DB = sqlite3.connect("./tweets")
+    CLASSIFIER = TweetClassifier()
+
+    # Train the classifier using sample of legislators' tweets.
+    #
+    # TO DO: select rows at random from sample. Partition sample into training
+    # and test subsets.
     
     for row in DB.execute("""select tweet, party, libscore from 
                           reps_score_tweets"""):
@@ -62,32 +73,15 @@ def train(DB, TRAINER):
         TRAINER.increment_tweet_class_count(row)
         TRAINER.classify_words(row)
 
-def classify(tweet):
-    """ Attempt to classify a tweet using its features. """
-
-    # Select tweets from the test set and classify!
+    CLASSIFIER.get_tweet_class_count('D')
+    CLASSIFIER.get_tweet_class_count('R')
+        
+    # Now attempt to classify tweets in test set.
     ...
     for tweet in test_set:
-        print "Prob. tweet is Dem: %i" % prob_class_given_features(tweet, "D")
-        print "Prob. tweet is GOP: %i" % prob_class_given_features(tweet, "R")
+        print "Prob. tweet is Dem: %i" % CLASSIFIER.classify(tweet, "D")
+        print "Prob. tweet is GOP: %i" % CLASSIFIER.classify(tweet, "R")
         print "Tweet's class: %s" % tweet[1]
-
-def main():
-    """
-    Main script.
-
-    NOTE: as a future extension, consider one trainer for each congressional
-    body. How do senators, reps tweet?
-    """
-      
-    DB = sqlite3.connect("./tweets")
-    TRAINER = TweetClassifier()
-
-    train(DB, TRAINER)
-
-    TRAINER.get_tweet_class_count('D')
-    TRAINER.get_tweet_class_count('R')
-
 
 # Run main() from bash.
 if "__name__" == "__main__":
